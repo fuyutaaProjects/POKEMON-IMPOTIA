5f8bf4eb73c30bf9bb69065bf3bc98fe5ba05864
n0l1um <fuyutaa.contact@gmail.com>
2024-06-28 10:07:57 +0200

    push

---
+++ IMPOTIA RMXP/Data/Tiled/Tilesets/common building interiors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="common building interiors" tilewidth="32" tileheight="32" tilecount="632" columns="8">
+ <image source="../Assets/common_buildings_interiors.png" width="256" height="2532"/>
+</tileset>
