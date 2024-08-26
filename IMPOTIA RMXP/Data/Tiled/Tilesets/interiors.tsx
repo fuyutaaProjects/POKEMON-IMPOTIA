69a13fb96c3805977c2af75221d04ffd596f2545
n0l1um <fuyutaa.contact@gmail.com>
2024-08-27 00:01:15 +0200

    a lot of progress

---
+++ IMPOTIA RMXP/Data/Tiled/Tilesets/interiors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="interiors" tilewidth="32" tileheight="32" tilecount="9984" columns="52">
+ <image source="../Assets/interiors.png" width="1664" height="6144"/>
+</tileset>
