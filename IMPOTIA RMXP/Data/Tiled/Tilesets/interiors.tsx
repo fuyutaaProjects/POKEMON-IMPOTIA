0768f60acf3209d086e48ea92f6fcde3cc4ef09b
fuyutaaProjects <113826935+fuyutaaProjects@users.noreply.github.com>
2024-09-28 22:30:15 +0200

    need fix 059

--- IMPOTIA RMXP/Data/Tiled/Tilesets/interiors.tsx
+++ IMPOTIA RMXP/Data/Tiled/Tilesets/interiors.tsx
@@ -1,4 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="interiors" tilewidth="32" tileheight="32" tilecount="9984" columns="52">
  <image source="../Assets/interiors.png" width="1664" height="6144"/>
+ <wangsets>
+  <wangset name="tapis" type="corner" tile="-1">
+   <wangcolor name="red carpet" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="4842" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="4843" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="4844" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="4845" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="4846" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="4894" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="4895" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="4896" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="4897" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="4898" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="4946" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="4947" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="4948" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
